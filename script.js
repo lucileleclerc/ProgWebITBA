c0bd769288b08718b6c867acbe26190a339ee869
lucileleclerc <lleclerc@itba.edu.ar>
2024-05-05 19:52:11 -0300

    add js

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 document.getElementById('suggSubmit').addEventListener('click', function() {
-    var newEl = document.getElementById('suggInput').value.trim();
+    var newEl1 = document.getElementById('suggTitleInput').value.trim();
+    var newEl2 = document.getElementById('suggAuthorInput').value.trim();
+    var newEl3 = document.getElementById('suggDescInput').value.trim();
 
-    if (newElement !== '') {
-        var listItem = document.createElement('li');
-        listItem.textContent = newElement;
-        document.getElementById('listeSugg').appendChild(listItem);
-        document.getElementById('suggInput').value = '';
+    if (newEl1 !== '') {
+        var titleItem = document.createElement('p');
+        var authorItem = document.createElement('p');
+        var descItem = document.createElement('p');
+        titleItem.textContent = newEl1;
+        authorItem.textContent = newEl2;
+        descItem.textContent = newEl3;
+        document.getElementById('listeTitleSugg').appendChild(titleItem);
+        document.getElementById('listeAuthorSugg').appendChild(authorItem);
+        document.getElementById('listeDescSugg').appendChild(descItem);
+        document.getElementById('suggTitleInput').value = '';
+        document.getElementById('suggAuthorInput').value = '';
+        document.getElementById('suggDescInput').value = '';
     } else {
-        alert('Veuillez entrer un élément.');
+        alert('Please write at least a title!');
     }
 });
